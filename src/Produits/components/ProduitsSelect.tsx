bc23c09523d755ebca92c460277afb29bb6802af
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-04-30 12:45:27 +0200

    Maj : Ajout Context

--- src/Produits/components/ProduitsSelect.tsx
+++ src/Produits/components/ProduitsSelect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { BASEURL } from "../../Constants/base_url";
 import { TResponse } from "../Types/TResponse";
 import { TProduit } from "../Types/TProduit";
@@ -6,9 +6,10 @@ import { ProduitView } from "./ProduitView";
 import { ProduitNew } from "./ProduitNew";
 import { DEFAULT_PRODUIT } from "../../Constants/DefaultProduit";
 import { NO_PRODUIT } from "../../Constants/NoProduit";
+import { ProduitsContext } from "../../Contexts/ProduitsContext";
 
 export function ProduitsSelect() {
-  const [produits, setProduits] = useState<TProduit[]>([]);
+  const {produits, setProduits} = useContext(ProduitsContext);
   const [produit, setProduit] = useState<TProduit>(NO_PRODUIT);
 
   useEffect(() => {
@@ -34,16 +35,18 @@ export function ProduitsSelect() {
     );
   });
 
+
+  
   return (
     <div>
       <div className="bg-primary rounded p-2 m-1">
         <div>{selectProduit}</div>
         <button onClick={()=>{setProduit(DEFAULT_PRODUIT)}}>Nouveau Produit</button>
       </div>
-      {produit.id > 0 && <ProduitView item={produit} />}
+      {produit.id > 0 && <ProduitView item={produit} setProduit={setProduit} />}
       {produit.id === -1 && (
         <div>
-          <ProduitNew produits={produits} setProduits={setProduits} setProduit={setProduit} />
+          <ProduitNew setProduit={setProduit} />
         </div>
       )}
     </div>
